1045e85c4c7c739c045b780d2048e9ec47b67448
zhangminvip <2107609379@qq.com>
2017-06-26 16:57:32 +0800

    封装中

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -49,54 +49,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getMovie() {
-        String baseUrl = "http://api.douban.com/v2/movie";
 
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .build();
-
-        MovieService movieService = retrofit.create(MovieService.class);
-//        Call<MovieEntity> call = moviceService.getTopMovie(0,10);
-//        call.enqueue(new Callback<MovieEntity>() {
-//            @Override
-//            public void onResponse(Call<MovieEntity> call, Response<MovieEntity> response) {
-//                resultTV.setText(response.body().toString());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<MovieEntity> call, Throwable t) {
-//                    resultTV.setText(t.getMessage());
-//            }
-//        });
-
-        movieService.getTopMovie(0, 10)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<MovieEntity>() {
-
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Toast.makeText(MainActivity.this, "Get Top Movie Completed", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        resultTV.setText(e.getMessage());
-                    }
-
-                    @Override
-                    public void onNext(MovieEntity movieEntity) {
-                        resultTV.setText(movieEntity.toString());
-                    }
-                });
     }
 
 }
